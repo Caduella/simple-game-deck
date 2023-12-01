351eedddaca37d5a39ebb0b4ef91805ee2d400e9
Caduella <146942933+Caduella@users.noreply.github.com>
2023-12-01 15:47:06 +0100

    Create pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'Chopchae';
-document.write(favoriteFood);
+// Create a variable called pokemonList and assign Pokemon data to it.
+var pokemonList = [
+	{name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}
+	{ name: 'Charmeleon', height: 1.1, type: 'Fire' },
+  { name: 'Wartortle', height: 1, type: 'Water' }
+]
