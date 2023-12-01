1d66ebed3e6d7f51c3d208ece39c3e9445202027
Caduella <146942933+Caduella@users.noreply.github.com>
2023-12-01 19:56:38 +0100

    Fix problems with correcting typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Create a variable called pokemonList and assign Pokemon data to it.
 var pokemonList = [
-	{name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}
-	{ name: 'Charmeleon', height: 1.1, type: 'Fire' },
-  { name: 'Wartortle', height: 1, type: 'Water' }
+	{name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+  {name: 'Wartortle', height: 1, type: 'water' },
+	{name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
 ]
