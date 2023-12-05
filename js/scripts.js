a8be8ab0d03e0aca0a3f587c98390487f953bcd6
Caduella <146942933+Caduella@users.noreply.github.com>
2023-12-05 23:27:03 +0100

    Created for loop condition

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,17 @@ var pokemonList = [
         types: ['fire', 'flying']
     },
 ]
+// Create a for loop to iterate over each pokemon in the array
+for (var i = 0; i < pokemonList.length; i++){
+    var pokemon = pokemonList[i]; //Select an individual pokemon from the pokemonList array
+    
+// Add conditional to check if the height is above 1
+if (pokemon.height > 1) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big! <br>"); 
+}
+else {document.write(pokemon.name + " (height: " + pokemon.height + ") <br>");
+}   
+}
+
+    
+
