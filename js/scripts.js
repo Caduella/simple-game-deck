e00ab3fe58c0eff5b6cad70a934796d53a3548cc
Caduella <146942933+Caduella@users.noreply.github.com>
2023-12-13 00:17:48 +0100

    IIFE and ForEach Loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
-// Create a variable called pokemonList and assign Pokemon data to it.
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Wartortle',
-        height: 1,
-        types: ['water']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire', 'flying']
-    },
-]
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Wartortle',
+            height: 1,
+            types: ['water']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire', 'flying']
+        },
+    ];
+  
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+   
+    return {
+      getAll: getAll,
+      add: add
+    };
+
+  })();
+
+
 // Use a forEach funciotn to iterate over each pokemon in the array
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write('<p>Name: ' + pokemon.name + '</p>');
     document.write('<p>Height: ' + pokemon.height + ' meters</p>');
     document.write('<p>Types: ' + pokemon.types.join(', ') + '</p>');
     document.write('<hr>'); // Separating each Pokemon's details
 });
-
-    
-
